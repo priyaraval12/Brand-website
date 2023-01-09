fe31add367acb7615a86944b9c3e7955290fc9f4
DESKTOP-ILPQD8P\Admin <priyanshiraval26@gmail.com>
2023-01-09 18:49:06 +0530

    changes fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route,useNavigationType,useLocation,} from "react-router-dom";
+import Home  from "./Pages/Home";
+import Signup  from "./Pages/Signup";
+import Login  from "./Pages/Login";
+import Loadmore  from "./Pages/Loadmore";
+
+import { useEffect } from "react";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    //TODO: Update meta titles and descriptions below
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/signup":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/login":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Loadmore" element={<Loadmore />} />
+    </Routes>
   );
 }
-
 export default App;
